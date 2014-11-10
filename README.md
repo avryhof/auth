7df608074c9003d372aab742d809d4d88f71ee73
Amos Vryhof <amos@vryhofresearch.com>
2014-11-10 13:37:16 -0500

    Add readme and License
    README.MD, LICENSE, and composer.json

---
+++ README.md
@@ -0,0 +1,33 @@
+Auth
+====
+
+An Authenication Class similar to Pear Auth ( http://pear.php.net/package/Auth/ )
+
+This class was created primarily because when I try to use pear/auth, composer fires a bunch of dependency issues I don't want to resolve. It is also designed to be lighter, and have less requirements than pear/auth, while maintaining somcompatible APIs.
+
+Example
+-------
+```php
+  require_once("auth.php");
+  or
+  require("vendor/autoload.php")
+
+  $auth_options = array(
+    "dsn" => "mysqli://".$dsn,
+    "table" => "users",
+    "usernamecol" => "email",
+    "passwordcol" => "password",
+    "db_fields" => array("id","name")
+  );
+  $auth = new Auth("Database", $auth_options, "", false);
+
+  $auth->start();
+
+  if (!$auth->checkAuth()) {
+      header("Location:".$baseurl."/login.php");
+  }
+
+Also related
+=============
+* https://github.com/avryhof/database
+* http://pear.php.net/package/Auth/docs
---
+++ auth.php
@@ -0,0 +1,159 @@
+<?
+/* ********************
+ * A Class that is somewhat compatible with pear auth.
+ * Designed to work with my own Database Abstraction layer 
+ * since the Pear Libs don't seem to be getting much love these days.
+ */
+ 
+ 	class Auth {
+		var $driver, $db, $loginfunction, $session_cache;
+		var $session_storage = "session";
+		var $authenticated = false;
+		var $session_array = array();
+		var $username = "";
+		var $opts = array(
+			"dsn"			=> "",
+			"table" 		=> "users",
+			"usernamecol" 	=> "username",
+			"passwordcol" 	=> "password"
+		);
+		
+		public function __construct($driver = "Database", $options, $loginFunction = false, $optional) {
+			$this->startSession();
+			$this->driver = $driver;
+			$this->loginfunction = $loginFunction;
+			$this->opts = $this->mergeoptions($this->opts, $options);
+			
+			/* We might want to be able to use other classes in the future */
+			if (class_exists($this->driver)) {
+				$this->db = new $this->driver($this->opts['dsn']);
+			}
+		}
+		
+		function start() {
+			$username = $this->db->escape($_POST['username']);
+			$password = $this->hasher($_POST['password']);
+			$users = $this->db->query("SELECT * FROM `".$this->opts['table']."` WHERE `".$this->opts['usernamecol']."` = '$username' AND `".$this->opts['passwordcol']."` = '$password'");
+			if ($users->num_rows > 0) {
+				$this->username = $username;
+				$this->authenticated = true;
+				$this->setSession("_auth_username", $this->username);
+				$this->setSession("_auth_authenticated", $this->authenticated);
+			} else {
+				$this->setSession("_auth_authenticated", $this->authenticated);
+				return $this->authenticated;
+			}
+		}
+		
+		function addUser($username, $password) {
+			$this->db->query("INSERT INTO `".$this->opts['table']."` (`".$this->opts['usernamecol']."`,`".$this->opts['passwordcol']."`) VALUES ('$username','".$this->hasher($password)."')");
+			return ($this->db->errno > 0 ? false: true);
+		}
+		
+		function changePassword ($username, $password) {
+			$this->db->query("UPDATE `".$this->opts['table']."` SET `".$this->opts['passwordcol']."` = '".$this->hasher($password)."' WHERE `".$this->opts['usernamecol']."` = '$username'");
+			return ($this->db->errno > 0 ? false: true);
+		}
+		
+		function removeUser ($username) {
+			$this->db->query("DELETE * FROM `".$this->opts['table']."` WHERE `".$this->opts['usernamecol']."` = '$username'");
+			return ($this->db->errno > 0 ? false: true);
+		}
+		
+		function listUsers() {
+			$users = $this->db->query("SELECT `".$this->opts['usernamecol']."` FROM `".$this->opts['table']."` ORDER BY `".$this->opts['usernamecol']."`");
+			$retn = array();
+			while($user = $users->fetch_assoc()) {
+				$un_col = $this->opts['usernamecol'];
+				$retn[] = $user[$un_col];
+			}
+			return $retn;
+		}
+		
+		function logout() {
+			$this->username = "";
+			$this->authenticated = false;
+			$this->setSession("_auth_username", $this->username);
+			$this->setSession("_auth_authenticated", $this->authenticated);
+		}
+		
+		function getUsername() {
+			return $this->username;
+		}
+				
+		function checkAuth() {
+			return ($this->authenticated == true ? true : false);
+		}
+		
+		function setSession($key, $value) {
+			if ($this->session_storage == "file") {
+				$this->session_array[$key] = $value;
+				file_put_contents($this->session_cache, serialize($this->session_array));
+			} else {
+				$_SESSION[$key] = $value;
+			}
+		}
+		
+		function getSession($key) {
+			if ($this->session_storage == "file") {
+				$this->session_array = unserialize(file_get_contents($this->session_cache));
+				return $this->session_array[$key];
+			} else {
+				return $_SESSION[$key];
+			}
+		}
+		
+		function startSession() {
+			if (session_status() == PHP_SESSION_DISABLED) {
+				if (!is_dir(getcwd()."/sessions")) { mkdir(getcwd()."/sessions"); }
+				$this->session_cache = getcwd()."/sessions/".md5($_SERVER['REMOTE_ADDR']);
+				$this->session_storage = "file";
+				if (file_exists($this->session_cache)) {
+					$this->session_array = unserialize(file_get_contents($this->session_cache));
+					if (is_array($this->session_array)) {
+						$this->username = $this->session_array['_auth_username'];
+						$this->authenticated = $this->session_array['_auth_authenticated'];
+					}
+				}
+			} elseif (session_status() == PHP_SESSION_NONE) {
+				session_start();
+				if (isset($_SESSION['_auth_authenticated'])) {
+					$this->authenticated = $this->session_array['_auth_authenticated'];
+					$this->username = $this->session_array['_auth_username'];
+				}
+			}
+		}
+		
+		function hasher($string, $hashfunction = false, $salt = "") {
+			if ($hashfunction == "crypt") {
+				/* See http://us3.php.net/manual/en/function.crypt.php */
+				return(crypt($string,$salt));
+			} elseif (function_exists($hashfunction)) {
+				/* Your custom hashing function should accept a string, and salt, and return a string */
+				return $hashfunction($string, $salt);
+			} else {
+				/* See http://us3.php.net/manual/en/function.hash.php */
+				return(hash((!$hashfunction ? "md5" : $hashfunction), $string));
+			}
+		}
+		
+		function mergeoptions($defaults, $changes) {
+			/* Only act if the changes are an array */
+			if (is_array($changes) && count($changes) > 0) {
+				/* Import defaults */
+				$newopts = $defaults;
+				foreach($changes as $o_key => $o_val) {
+					/* Change the value if it exists, add it if it doesn't */
+					if (array_key_exists($o_key,$defaults)) {
+						$newopts[$o_key] = $o_val;
+					} else {
+						$newopts[$o_key] = $o_val;
+					}
+				}
+				return $newopts;
+			} else {
+				return $defaults;
+			}
+		}
+  }
+?>
